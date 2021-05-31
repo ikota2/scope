a792c1aafb351b41723210c8206609bab7d880e5
Ivan Kotov <iamivankotov@gmail.com>
2021-05-31 21:24:30 +0300

    next.config added

---
+++ next.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    //...other configs
+    webpack: (config) => {
+        config.module.rules.push({
+                test: [/\.svg$/, /\.woff$/, /\.png$/],
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[hash:8].[ext]',
+                    publicPath: `/_next/static/images/`, //specify the base path
+                    outputPath: 'static/images', //and output path
+                }
+        })
+        return config;
+    },
+    future: {
+        webpack5: true,
+      },
+ 
+}
