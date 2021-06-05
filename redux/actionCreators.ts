c9efa12c06c7a6836e2872f6e2a18bfbab51ada1
Ivan Kotov <iamivankotov@gmail.com>
2021-06-05 18:39:26 +0300

    actionCreators made in redux folder

---
+++ redux/actionCreators.ts
@@ -0,0 +1,9 @@
+export const setStartingDistance = (value: number) => ({ type: 'SET_FINISH_DISTANCE', value });
+
+export const setFinishDistance = (value: number) => ({ type: 'SET_FINISH_DISTANCE', value });
+
+export const setQuantityWords = (value: number) => ({ type: 'SET_QUANTITY_LETTERS', value });
+
+export const setQuantityLetters = (value: number) => ({ type: 'SET_QUANTITY_LETTERS', value });
+
+export const setSpeed = (value: number) => ({ type: 'SET_SPEED', value });
